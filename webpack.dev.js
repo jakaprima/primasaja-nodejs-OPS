5478020bc754d90440cde21016fc3c19a94798b2
jakaprima <jakaprima123@gmail.com>
2017-11-28 06:41:21 +0700

    react with webpack-dev-middleware

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,66 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
-const common = require('./webpack.common.js');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const common = require('./webpack.common.js');
 
 
 
-module.exports = merge(common, {
-	devServer: {
-		contentBase: './dist',
-		port: 4000,
-		historyApiFallback: true,
-		hot: true
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	]
-});
+// module.exports = merge(common, {
+// 	devServer: {
+// 		contentBase: './dist',
+// 		port: 4000,
+// 		historyApiFallback: true,
+// 		hot: true
+// 	},
+// 	plugins: [
+// 		new webpack.HotModuleReplacementPlugin()
+// 	]
+// });
+
+
+
+module.exports = {
+    entry: './src/client/index.jsx',
+    output: {
+        filename: 'index_bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'http://0.0.0.0:8080/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: path.join(__dirname, '/src/client'),
+                exclude: '/node_modules',
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default']
+        }),
+        // new CleanWebpackPlugin(['dist']),
+    ],
+    devtool: 'cheap-module-eval-source-map',
+    watch: true
+};
