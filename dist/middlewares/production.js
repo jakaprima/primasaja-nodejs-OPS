b9e69ab7bacc60ee4b1d2905f52463f8e7e4e80c
jakaprima <jakaprima123@gamil.com>
2018-02-06 16:15:54 +0700

    server fix

--- dist/middlewares/production.js
+++ dist/middlewares/production.js
@@ -2,12 +2,12 @@
 var express = require('express');
 var compression = require('compression');
 
-var clientBuildPath = resolve(__dirname, '..', '..', 'client');
+var clientBuildPath = resolve(__dirname, '..', 'client');
 
 module.exports = function setup(app) {
   app.use(compression());
   app.use('/', express.static(clientBuildPath));
 
-  // all other requests be handled by UI itself
+  // untuk handle semua User Interface
   app.get('*', function (req, res) {return res.sendFile(resolve(clientBuildPath, 'index.html'));});
 };;var _temp = function () {if (typeof __REACT_HOT_LOADER__ === 'undefined') {return;}__REACT_HOT_LOADER__.register(clientBuildPath, 'clientBuildPath', 'src/server/middlewares/production.js');}();;
